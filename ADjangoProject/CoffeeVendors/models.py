bef8148f058b3c4973df81e26f8e811641b7e4a4
ElenaKusevska <elena.kusevska@gmail.com>
2024-04-26 23:54:48 +0200

    add vendor type to cofee vendor

--- ADjangoProject/CoffeeVendors/models.py
+++ ADjangoProject/CoffeeVendors/models.py
@@ -3,9 +3,21 @@ from django.db import models
 # from django.contrib.gis.geos import Point
 from django.contrib.auth.models import User
 
+
+class VendorType(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class CoffeeVendor(models.Model):
 
     name = models.CharField(max_length=100)
+    vendor_type = models.ForeignKey(VendorType, null=True, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
     description = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True) 
