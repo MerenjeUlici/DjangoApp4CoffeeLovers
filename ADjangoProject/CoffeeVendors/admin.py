b46d3c9cfb24aad2ba5edb2ece38207ae2fbebe1
ElenaKusevska <elena.kusevska@gmail.com>
2024-05-02 00:00:42 +0200

    register VendorType with admin

--- ADjangoProject/CoffeeVendors/admin.py
+++ ADjangoProject/CoffeeVendors/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import CoffeeVendor, CoffeeVendorReview
+from .models import CoffeeVendor, VendorType, CoffeeVendorReview
 
 admin.site.register(CoffeeVendor)
+admin.site.register(VendorType)
 admin.site.register(CoffeeVendorReview)
