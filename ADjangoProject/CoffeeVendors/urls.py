7f886563f7916d0b4b32abeae9cbf90bc9253221
ElenaKusevska <elena.kusevska@gmail.com>
2024-05-02 00:05:05 +0200

    modify urls for vendors and api

--- ADjangoProject/CoffeeVendors/urls.py
+++ ADjangoProject/CoffeeVendors/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.VendorsList.as_view(), name='vendors-list'),
-    path('vendor/<int:pk>', views.VendorDetail.as_view(), name='vendors-by-id')
+    path('vendors/', views.VendorsList.as_view(), name='vendors-list'),
+    path('vendors/<int:pk>', views.VendorDetail.as_view(), name='vendors-by-id')
 
 ]
